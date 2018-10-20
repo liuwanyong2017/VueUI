949d1486f23bfc3419c9f711e22a4f04a7674a2d
liuwanyong2017 <2674706698@qq.com>
2018-10-20 23:27:38 +0800

    button 基本流程走了一遍

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import Button from './button'
+import Icon from './icon'
+import ButtonGroup from './button-group'
 
-Vue.component('v-button',Button)
-
+Vue.component('v-button',Button);
+Vue.component('v-icon',Icon);
+Vue.component('v-button-group',ButtonGroup);
 new Vue({
     el:"#app",
     data : {
-        msg:'哈哈哈'
+        msg:'哈哈哈',
+        loading1 : true,
+        loading2 : true,
+        loading3 : true
     }
-})
+
+});
--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,32 @@
 <template>
-    <button class="v-button">按钮</button>
+    <button class="v-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+        <v-icon v-if="icon && !loading" :name="icon" class="icon"></v-icon>
+        <v-icon v-if="loading" name="loading" class="icon loading"></v-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+       // props: ['icon','icon-position']
+        props : {
+            icon : {} ,
+            loading : {
+                type : Boolean,
+                default : false
+            },
+            iconPosition : {
+                type : String,
+                default : 'left',
+                validator(value){
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
+    }
 
 </script>
 
@@ -15,6 +38,7 @@
         border: 1px solid var(--border-color);
         background: var(--button-bg);
         border-radius: var(--button-radius);
+        display:inline-flex;justify-content:center;align-items:center;
         &:focus {
             outline: none;
         }
@@ -24,6 +48,25 @@
         &:active {
             background-color: var(--button-active-bg);
         }
+        > .icon {order : 1;margin-right:.3em;}
+        > .content {order : 2;}
+        &.icon-right {
+            > .icon {order: 2;margin-left:.3em;margin-right:0;}
+            > .content {order:1;}
+        }
+        vertical-align : middle;
+        @keyframes loading {
+            0% {
+                 transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+        .loading {
+            animation: loading 1.5s infinite linear;
+        }
+
     }
 
 
